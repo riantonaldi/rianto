197d5532e35e7fd5752b62138ed65ec864324117
riantonaldi <68768758+riantonaldi@users.noreply.github.com>
2020-07-27 14:39:34 +0700

    Add files via upload

---
+++ guipraktek.sql
@@ -0,0 +1,225 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 25 Jul 2020 pada 13.55
+-- Versi server: 10.1.38-MariaDB
+-- Versi PHP: 7.3.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `guipraktek`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `barang`
+--
+
+CREATE TABLE `barang` (
+  `barcode` varchar(30) NOT NULL,
+  `nama_barang` varchar(200) NOT NULL,
+  `harga` int(7) NOT NULL,
+  `stok` int(3) NOT NULL,
+  `keterangan` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `barang`
+--
+
+INSERT INTO `barang` (`barcode`, `nama_barang`, `harga`, `stok`, `keterangan`) VALUES
+('101921', 'Indomie', 5000, 20, ''),
+('111a', 'Pop Mie', 5000, 84, ''),
+('12345', 'photo copy', 100, 993, '');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `level`
+--
+
+CREATE TABLE `level` (
+  `no` int(2) NOT NULL,
+  `nama_lvl` varchar(100) NOT NULL,
+  `keterangan` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `level`
+--
+
+INSERT INTO `level` (`no`, `nama_lvl`, `keterangan`) VALUES
+(1, 'Admin', 'Fungsinya untuk administrator sistem'),
+(2, 'Pegawai', 'Fungsinya Untuk Tulis Menulis');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pengguna`
+--
+
+CREATE TABLE `pengguna` (
+  `id` int(2) NOT NULL,
+  `nama` varchar(200) NOT NULL,
+  `sandi` varchar(300) NOT NULL,
+  `level` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `pengguna`
+--
+
+INSERT INTO `pengguna` (`id`, `nama`, `sandi`, `level`) VALUES
+(1, 'tif2020', '550384d6bba90df48bb75955f1bda014', 1),
+(4, 'test1', '098F6BCD4621D373CADE4E832627B4F6', 2),
+(5, 'admin', '21232f297a57a5a743894a0e4a801fc3', 1),
+(6, 'ari', 'FC292BD7DF071858C2D0F955545673C1', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_tmp_beli`
+--
+
+CREATE TABLE `tbl_tmp_beli` (
+  `id_tmp` int(11) NOT NULL,
+  `barcode` varchar(30) NOT NULL,
+  `nama_barang` varchar(200) NOT NULL,
+  `hsatuan` int(11) NOT NULL,
+  `jumlah_beli` int(11) NOT NULL,
+  `harga` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Trigger `tbl_tmp_beli`
+--
+DELIMITER $$
+CREATE TRIGGER `batal` AFTER DELETE ON `tbl_tmp_beli` FOR EACH ROW BEGIN
+UPDATE barang SET stok = stok + OLD.jumlah_beli
+WHERE barcode = OLD.barcode;
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `beli` AFTER INSERT ON `tbl_tmp_beli` FOR EACH ROW BEGIN 
+UPDATE barang SET stok = stok - new.jumlah_beli 
+WHERE barcode = new.`barcode`; 
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `faktur` varchar(11) NOT NULL,
+  `barcode` varchar(30) NOT NULL,
+  `nama_barang` varchar(300) NOT NULL,
+  `hsatuan` int(11) NOT NULL,
+  `jumlah_beli` int(11) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `bayar` int(11) NOT NULL,
+  `kembalian` int(11) NOT NULL,
+  `tanggal` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`barcode`);
+
+--
+-- Indeks untuk tabel `level`
+--
+ALTER TABLE `level`
+  ADD PRIMARY KEY (`no`);
+
+--
+-- Indeks untuk tabel `pengguna`
+--
+ALTER TABLE `pengguna`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `level` (`level`);
+
+--
+-- Indeks untuk tabel `tbl_tmp_beli`
+--
+ALTER TABLE `tbl_tmp_beli`
+  ADD PRIMARY KEY (`id_tmp`),
+  ADD KEY `barcode` (`barcode`);
+
+--
+-- Indeks untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD KEY `barcode` (`barcode`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `level`
+--
+ALTER TABLE `level`
+  MODIFY `no` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `pengguna`
+--
+ALTER TABLE `pengguna`
+  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_tmp_beli`
+--
+ALTER TABLE `tbl_tmp_beli`
+  MODIFY `id_tmp` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `pengguna`
+--
+ALTER TABLE `pengguna`
+  ADD CONSTRAINT `pengguna_ibfk_1` FOREIGN KEY (`level`) REFERENCES `level` (`no`);
+
+--
+-- Ketidakleluasaan untuk tabel `tbl_tmp_beli`
+--
+ALTER TABLE `tbl_tmp_beli`
+  ADD CONSTRAINT `tbl_tmp_beli_ibfk_1` FOREIGN KEY (`barcode`) REFERENCES `barang` (`barcode`);
+
+--
+-- Ketidakleluasaan untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`barcode`) REFERENCES `barang` (`barcode`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
